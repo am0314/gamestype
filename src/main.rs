69d46c95019b3d16f4ad4214e3052d9033d32ddf
murphy <am031447@gmail.com>
2021-11-02 18:05:22 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
     println!("Hello, world!");
 }
+type StageResult = Result<(), String>;
+
+pub trait TService {
+    fn start(self) -> StageResult;
+    fn handle(self) -> StageResult;
+    fn shotdown(self);
+}
+
+pub struct ServiceContext {
+    pub ServiceRuntime: Vec<Box<dyn TService>>,
+}
+
+impl ServiceContext {
+    pub fn Run(&mut self) {
+        for c in &mut self.ServiceRuntime.iter() {
+            match c.start() {
+                Ok(_) => (),
+                Err(s) => println!("{}", &s),
+            };
+        }
+    }
+}
+
+
+pub struct ServiceMember {}
+
+impl ServiceMember {
+    pub fn start(self) {}
+}
